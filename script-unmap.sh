f888ace86c0bc92549424dd258db690873b8ee7d
Anderson Serra <61382144+andersonserra@users.noreply.github.com>
2022-02-09 22:03:14 -0300

    Update script-unmap.sh

--- script-unmap.sh
+++ script-unmap.sh
@@ -21,32 +21,25 @@ OUTPUT="$WORKDIR/output.txt"
 if [ -d "$WORKDIR" ]; then
   # Take action if $DIR exists. #
   echo "Starting process unmap datastores..."
- else
+else
   echo "Folder does not exist! Creating folder!"
   mkdir $WORKDIR
 fi
 
-
+#In this step you need atencion for choose only specifics datastores. Use command:   egrep -v "(pattern1|pattern2|...)"
 #Filter only datastores VMFS datastores, or specifics datastores:
-$ESXCLI storage vmfs extent list |egrep -v "(datastore_host53|UUID|-------------)" | awk '{print $1}' > $WORKDIR/volume_label
+$ESXCLI storage vmfs extent list |egrep -v "(ignore_examples1|ignore_examples2|UUID|-------------)" | awk '{print $1}' > $WORKDIR/volume_label
 
 
 echo "" > $OUTPUT
-
 echo "PATH NAME,START,END" >> $OUTPUT
-
 for pathname  in `cat $WORKDIR/volume_label`;
 do
-
-starttime=$(date +"%Y-%m-%d %H:%M:%S")
-
-$ESXCLI storage vmfs unmap -l $pathname
-wait
-
-endtime=$(date +"%Y-%m-%d %H:%M:%S")
-
-echo "$pathname,$starttime,$endtime" >> $OUTPUT
-
+  starttime=$(date +"%Y-%m-%d %H:%M:%S")
+  $ESXCLI storage vmfs unmap -l $pathname
+  wait
+  endtime=$(date +"%Y-%m-%d %H:%M:%S")
+  echo "$pathname,$starttime,$endtime" >> $OUTPUT
 done
 
 
